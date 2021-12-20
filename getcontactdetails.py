66ff80c752f1fd9cb64fd40f45c57393728e4cb9
Pacificmiracle <87436695+Pacificmiracle@users.noreply.github.com>
2021-12-20 11:46:26 +0545

    Update getcontactdetails.py

--- getcontactdetails.py
+++ getcontactdetails.py
@@ -4,7 +4,7 @@ from requests_oauthlib import OAuth2
 
 code = requests.post("http://18.136.220.168:1000/api/", auth=OAuth2(client_id=r'3', client_secret=
 r'TJGBPvHd6Zd2vmDR8DpwUZfXcAiOX5SuzHabS7ZO',username='sabin.maharjan@logicabeans.com',
-password='test'))
+password='abcdqwerty'))
 resp = requests.get("http://18.136.220.168:1000/api/contact-us")
 print(resp)
 json_response = resp.json()
