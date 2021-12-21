c710dde959ab4adb359999f6aa02a29da1d4d568
Pacificmiracle <87436695+Pacificmiracle@users.noreply.github.com>
2021-12-21 11:53:30 +0545

    Add files via upload

---
+++ weblocators.py
@@ -0,0 +1,13 @@
+class Locators():
+
+    # Login Page Objects
+    email_mobile_textbox_xpath = "//input[@placeholder='Email or Mobile No.']"
+    password_textbox_xpath = "//input[@placeholder='Password']"
+    login_btn_xpath = "//button[normalize-space()='Login']"
+
+    # Home Page Objects
+    profile_class = "img-circle image-circle-active image-style"
+    logout_link_xpath = "/html/body/div[1]/app-root/app-header/nav/ul[2]/li/a/i"
+
+
+
